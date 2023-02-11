62fe3870b3a8aee737ff2776509b5326dee41bec
Arnaud Morisset <28533995+arnaudmorisset@users.noreply.github.com>
2023-02-11 10:19:21 +0100

    Download each source audio in a dedicated folder

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 readarray -t sources <<< "$(cat ./sources)"
 
+[ ! -d "./out" ] && mkdir ./out
+
 for source in "${sources[@]}"
 do
-  echo "$source"
+  (cd ./out && \
+  IFS=';' read -ra src <<< "$source" && \
+  echo "Downloading ${src[0]} from YouTube..." && \
+  youtube-dl -x --audio-format wav "${src[1]}")
 done
