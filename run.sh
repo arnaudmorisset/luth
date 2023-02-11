af9afb64738d9599e58f9a6b0685cb58861b2db9
Arnaud Morisset <28533995+arnaudmorisset@users.noreply.github.com>
2023-02-11 11:01:44 +0100

    Override YT file name and prepare S3 upload

--- run.sh
+++ run.sh
@@ -12,5 +12,9 @@ while read -r src ; do
   (cd ./out && \
   IFS=';' read -ra src <<< "$src" && \
   echo "Downloading ${src[0]} from YouTube..." && \
-  youtube-dl -x --audio-format wav "${src[1]}")
+  youtube-dl -x --audio-format wav -o "${src[0]}.wav" "${src[1]}" && \
+  echo "${src[0]} was downloaded inside the out/ folder")
+  # echo "Uploading ${src[0]} to S3 bucket ${LUTH_S3_BUCKET}..." && \
+  # aws s3 mv "${src[0]}.wav" "s3://${LUTH_S3_BUCKET}" && \
+  # echo "${src[0]} was uploaded to S3 bucket ${LUTH_S3_BUCKET}")
 done < ./sources
